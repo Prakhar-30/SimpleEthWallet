bbf1ab4081a1e23e33b13e4cd6ab52a5c62b2588
PRAKHAR SRIVASTAVA <130750923+Prakhar-30@users.noreply.github.com>
2024-06-28 15:20:07 +0530

    Update WalletEths.sol

--- WalletEths.sol
+++ WalletEths.sol
@@ -1,2 +1,8 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.3;
+contract EtherWallet{
+    address payable public owner;
+    constructor(){
+        owner= payable(msg.sender);
+    }
+    receive() external payable { }  
